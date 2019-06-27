769eaeed2ee0b012f76200b68d52e858e36f921f
Chidi Okoye <chidizy4@yahoo.com>
2019-06-27 11:55:09 +0100

    Page rendering

--- book-keeping/src/App.js
+++ book-keeping/src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import Book from './components/book/book';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "book-wrapper">
+      <Header/>
+      <div className= "row">
+        <div className = "col-md-4">
+          <Book/>  
+        </div>
+        <div className = "col-md-4">
+        <Book/> 
+        </div>
+        <div className = "col-md-4">
+        <Book/> 
+        </div>
+        <div className = "col-md-4">
+          <Book/>  
+        </div>
+        <div className = "col-md-4">
+        <Book/> 
+        </div>
+        <div className = "col-md-4">
+        <Book/> 
+        </div>
+      </div>
+      
+      </div>
     </div>
   );
 }
